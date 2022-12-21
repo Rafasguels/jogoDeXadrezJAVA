2a9ecb84ccfcdcd01f5769a3ab48a6d23a00d658
Rafasguels <rafael.vieira159@outlook.com>
2022-12-20 22:28:26 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -44,7 +44,7 @@ public class UI {
 		return new ChessPosition(column, row);
 		}
 		catch (RuntimeException e) {
-			throw new InputMismatchException("Error reading ChessPosition. Valid values are from at to h8.");
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 at to h8.");
 		}
 	}
 	
